9db3ee6690161e4d0baff70dbdc912c6519c4823
atad <1585232641@qq.com>
2022-07-10 16:30:13 +0800

    修改样式

--- static/js/ec_right1.js
+++ static/js/ec_right1.js
@@ -18,13 +18,18 @@ var ec_right1_option = {
         },
         left : 'left'
     },
-      color: ['#3398DB'],
-        tooltip: {
-            trigger: 'axis',
-            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-            }
-        },
+    color: ['#3398DB'],
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        }
+    },
+    grid: {
+        x: 65, //图表左上角到左边界的距离
+        y: 38, //图表左上角到上边界的距离
+        x2: 65, //图表右下角到右边界的距离
+    },
     xAxis: {
         type: 'value',
         axisLabel: {
